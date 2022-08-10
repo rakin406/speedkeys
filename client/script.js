2c077bd3f2f8a7e44ed74fd026506b1452bd94ee
Rakin Rahman <rakinhacker@gmail.com>
2022-08-10 18:55:55 +0600

    Match keypress with character in textbox

--- client/script.js
+++ client/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let textbox = document.getElementById("textbox");
+let tbString = textbox.value.trim();
+let i = 0;
+
+// Record every key press
+textbox.addEventListener("keydown", (event) => {
+    // Check if key press matches textarea character
+    if (event.key === tbString[i]) {
+        console.log(event.key); // debugging
+        ++i;
+    }
+})
