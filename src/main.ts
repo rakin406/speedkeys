58cb445eb591f5c0262cb7e474534c3535960df9
Rakin Rahman <rakinhacker@gmail.com>
2022-08-10 19:20:04 +0600

    Add compression

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
+import * as compression from "compression";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
+    app.use(compression());
 
     await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
