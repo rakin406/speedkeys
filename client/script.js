62b97fd72cea79f3adc7ffe1e40f850752930cf5
Rakin Rahman <rakinhacker@gmail.com>
2022-08-10 23:32:09 +0600

    Wait for the user to give correct input

--- client/script.js
+++ client/script.js
@@ -1,24 +1,18 @@
-let text = document.getElementsByClassName("text");
-let i = 0;
-let correct;
+let text = document.getElementsByClassName("text"); // characters
+let i = 0; // character index
+let correct = true; // boolean for detecting correct and wrong input
 
-// Record every key press
 this.addEventListener("keydown", (event) => {
     if (i < text.length) {
-        // Check if key press matches character
         if (event.key === text[i].innerText) {
-            // Don't skip wrong input. The user HAS to press the correct key
-            // or else he can't move on to the next. The red color must stay
-            // if the user gave wrong input BEFORE his fix.
-            if (correct === true) {
-                text[i].style.color = "#808080";
+            if (correct == true) {
+                text[i].style.color = "#808080"; // grey if correct
+            } else {
+                text[i].style.color = "#ff0000"; // red if wrong
             }
             correct = true;
-            ++i;
+            ++i; // move on to the next character in the text
         } else {
-            // TODO: Wait for the user to type the correct key and THEN make
-            // the letter red, NOT before.
-            text[i].style.color = "#ff0000";
             correct = false;
         }
     }
