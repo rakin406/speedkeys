f34886783db91c70b51bc05f20647633bbc8dbe8
Rakin Rahman <rakinhacker@gmail.com>
2022-08-10 22:11:54 +0600

    Make it work

--- client/script.js
+++ client/script.js
@@ -1,12 +1,13 @@
-let textbox = document.getElementById("textbox");
-let tbString = textbox.value.trim();
+let text = document.getElementsByClassName("text");
 let i = 0;
 
 // Record every key press
-textbox.addEventListener("keydown", (event) => {
-    // Check if key press matches textarea character
-    if (event.key === tbString[i]) {
-        console.log(event.key); // debugging
-        ++i;
+this.addEventListener("keydown", (event) => {
+    if (i < text.length) {
+        // Check if key press matches character
+        if (event.key === text[i].innerText) {
+            console.log(event.key); // debugging
+            ++i;
+        }
     }
-})
+});
