7e07bd5ff6824b1e78eabe09a2ab24bbbc5bfa86
Rakin Rahman <rakinhacker@gmail.com>
2022-08-11 00:16:46 +0600

    Ignore special characters

--- client/script.js
+++ client/script.js
@@ -2,8 +2,15 @@ let text = document.getElementsByClassName("text"); // characters
 let i = 0; // character index
 let correct = true; // boolean for detecting correct and wrong input
 
+const EXCLUDED_CHARS = ["Control", "Meta", "Alt", "ContextMenu", "ArrowLeft",
+    "ArrowRight", "ArrowDown", "ArrowUp", "Shift", "CapsLock", "Backspace",
+    "Insert", "Delete", "Home", "End", "PageUp", "PageDown", "Escape"];
+
 this.addEventListener("keydown", (event) => {
-    if (i < text.length) {
+    // Ensure that key press is NOT an excluded special character and also
+    // ensure that text length is always higher than the index.
+    if (!EXCLUDED_CHARS.includes(event.key) && i < text.length) {
+        // Check if key press matches the character from text
         if (event.key === text[i].innerText) {
             if (correct == true) {
                 text[i].style.color = "#808080"; // grey if correct
