feff2ec02142b09584d00601b6518024d2b6dcd6
Rakin Rahman <rakinhacker@gmail.com>
2022-08-10 20:08:53 +0600

    Switch to fastify for optimization

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 import * as compression from "compression";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter()
+    );
     app.setGlobalPrefix("api");
     app.use(compression());
 
-    await app.listen(3000);
+    await app.listen(3000, "0.0.0.0");
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
